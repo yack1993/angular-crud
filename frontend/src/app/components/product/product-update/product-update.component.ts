af0e2a8d92e68e8ef597c80cee3db5d2581e3e37
Eduardo_ <eduardo.geraldo@galeriapage.com.br>
2021-07-12 14:56:19 -0300

    cadastro produtos

---
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -0,0 +1,35 @@
+import { ProductService } from './../product.service';
+import { Product } from './../product.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-update',
+  templateUrl: './product-update.component.html',
+  styleUrls: ['./product-update.component.css']
+})
+export class ProductUpdateComponent implements OnInit {
+
+  product!: Product;
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')|| '';
+    this.productService.readById(id).subscribe(product => {
+      this.product = product;
+    });
+  }
+
+  updateProduct():void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Atualizadc com Sucesso!');
+      this.router.navigate(['/products']);
+    })
+    
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+
+}
---
+++ frontend/src/app/components/product/product.model.ts
@@ -0,0 +1,6 @@
+export interface Product {
+    //? -> opcional
+    id?: number,
+    name: string,
+    price: number
+}
